cbf71586e1174ad0f372c198a41347bc1ece7932
Michael Matus <michael@matus.at>
2021-12-07 15:22:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bev.Instruments.Msc15;
+
+namespace MSC15test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var device = new Msc15("MSC15_0");
+
+            Console.WriteLine($"ID: {device.InstrumentID}");
+            //device.MeasureDark();
+
+            for (int i = 0; i < 10; i++)
+            {
+                device.Measure();
+                Console.WriteLine($"{i+1,4}:   {device.CctValue:F0} K    {device.PhotopicValue:F2} lx  {device.GetLastIntegrationTime():F4} s");
+            }
+
+            Console.WriteLine();
+            var spec = device.GetSpectrum();
+
+            for (int i = 0; i < spec.Length; i++)
+            {
+                Console.WriteLine($"{i,4} {spec[i].Wavelength:F2} nm  ->  {1000*spec[i].Irradiance:F6}");
+            }
+        }
+    }
+}
