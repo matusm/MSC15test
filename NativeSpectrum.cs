cc3b6ce0547144df8a29e89688331219f0f41ff8
Michael Matus <michael@matus.at>
2021-12-24 21:06:50 +0100

    Class for nativ spectrum

---
+++ NativeSpectrum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bev.Instruments.Msc15;
+
+namespace MSC15test
+{
+    public class NativeSpectrum
+    {
+        private const int numberPixel = 288;
+
+        private SpectralQuantityValue[] nativeSpectrum = new SpectralQuantityValue[numberPixel];
+
+        public SpectralQuantityValue[] Spectrum => nativeSpectrum;
+
+        public void Update(SpectralValue[] spectrum)
+        {
+            if (spectrum.Length != numberPixel)
+                return;
+            for (int i = 0; i < spectrum.Length; i++)
+            {
+                nativeSpectrum[i].UpdateValue(spectrum[i]);
+            }
+        }
+
+    }
+}
---
+++ SpectralQuantityValue.cs
@@ -0,0 +1,25 @@
+﻿using Bev.Instruments.Msc15;
+using At.Matus.StatisticPod;
+
+namespace MSC15test
+{
+    public class SpectralQuantityValue
+    {
+        public double AverageValue => valuePod.AverageValue;
+        public double StandardDeviation => valuePod.StandardDeviation;
+        public double Wavelength { get; private set; }
+
+        private StatisticPod valuePod = new StatisticPod();
+
+        public void UpdateValue(SpectralValue sv)
+        {
+            UpdateValue(sv.Irradiance);
+            Wavelength = sv.Wavelength;
+        }
+
+        public void UpdateValue(double value)
+        {
+            valuePod.Update(value);
+        }
+    }
+}
